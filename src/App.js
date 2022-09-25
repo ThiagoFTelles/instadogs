30e63e3621e0a84ac4d3dcb79ffbed875092f37b
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-25 17:54:25 -0300

    feat: create Home.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
