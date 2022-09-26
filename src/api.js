ac60f306a417727c08e41f64440100988f49a2d2
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-25 23:00:58 -0300

    feat: create api.js helper

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const API_URL = 'https://dogsapi.origamid.dev/json';
+
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + 'jwt-auth/v1/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  };
+}
