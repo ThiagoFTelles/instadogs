aea899f9eb2e3903e22dbfabbbf7ffca773027df
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-26 18:10:17 -0300

    feat: create UserHeaderNav style module and import images

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import { ReactComponent as Pictures } from '../../Assets/feed.svg';
+import { ReactComponent as Statistics } from '../../Assets/estatisticas.svg';
+import { ReactComponent as Add } from '../../Assets/adicionar.svg';
+import { ReactComponent as Exit } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
 
   return (
-    <nav>
-      <NavLink to="/user">Minhas Fotos</NavLink>
-      <NavLink to="/user/statistics">Estatísticas</NavLink>
-      <NavLink to="/user/post">Adicionar Fotos</NavLink>
-      <button onClick={userLogout}>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to="/user">
+        <Pictures />
+        Minhas Fotos
+      </NavLink>
+      <NavLink to="/user/statistics">
+        <Statistics />
+        Estatísticas
+      </NavLink>
+      <NavLink to="/user/post">
+        <Add />
+        Adicionar Fotos
+      </NavLink>
+      <button onClick={userLogout}>
+        <Exit />
+        Sair
+      </button>
     </nav>
   );
 };
