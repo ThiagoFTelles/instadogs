7e4d570d6f2e9ce6417395796e3e04d0d0a8e1aa
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-26 16:36:41 -0300

    feat: add password validation.

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Email inválido',
   },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    message:
+      'A senha precisa ter: Ao menos 1 letra maiúscula, 1 minúscula, 1 número, 1 caractere especial e no mínimo 8 caracteres no total.',
+  },
 };
 
 const useForm = (type) => {
