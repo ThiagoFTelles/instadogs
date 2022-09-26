1da71d764f1232405d9f3508ded90c85228c37b4
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-26 16:56:37 -0300

    feat: create user signup function

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -2,14 +2,22 @@ import React from 'react';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
 
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
   const password = useForm('password');
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    console.log(response);
   }
 
   return (
