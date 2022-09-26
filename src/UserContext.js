30204e0fdc5f5d09f59e9754d8a0bf6fce7bf785
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-26 14:02:08 -0300

    feat: navigate to the right page.

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = React.createContext();
 
@@ -8,6 +9,19 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
+
+  const userLogout = React.useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem('token');
+      navigate('/login');
+    },
+    [navigate],
+  );
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -30,20 +44,13 @@ export const UserStorage = ({ children }) => {
 
       window.localStorage.setItem('token', token);
       await getUser(token);
+      navigate('/user');
     } catch (error) {
       setError(error.message);
       setLogin(false);
     } finally {
       setLoading(false);
     }
-  }
-
-  async function userLogout() {
-    setData(null);
-    setError(null);
-    setLoading(false);
-    setLogin(false);
-    window.localStorage.removeItem('token');
   }
 
   React.useEffect(() => {
@@ -67,7 +74,7 @@ export const UserStorage = ({ children }) => {
       }
     }
     autoLogin();
-  }, []);
+  }, [userLogout]);
 
   return (
     <UserContext.Provider
