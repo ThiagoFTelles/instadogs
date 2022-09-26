18b7744b48c03a1479329f7a379782286c7d06e8
ThiagoFTelles <atendimento.telles@gmail.com>
2022-09-26 17:56:53 -0300

    feat: create UserHeaderNav

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserHeaderNav from './UserHeaderNav';
 
 const UserHeader = () => {
   return (
--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 
 const UserHeaderNav = () => {
-  return <div>UserHeaderNav</div>;
+  const { userLogout } = React.useContext(UserContext);
+
+  return (
+    <nav>
+      <NavLink to="/user">Minhas Fotos</NavLink>
+      <NavLink to="/user/statistics">Estatísticas</NavLink>
+      <NavLink to="/user/post">Adicionar Fotos</NavLink>
+      <button onClick={userLogout}>Sair</button>
+    </nav>
+  );
 };
 
 export default UserHeaderNav;
